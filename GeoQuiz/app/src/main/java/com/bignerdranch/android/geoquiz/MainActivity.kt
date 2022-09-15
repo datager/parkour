92c293b84059c93160cd34fcebe8dbea390696b2
y <daska110@126.com>
2022-09-15 11:49:38 +0800

    feat: view model and on save instance state

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,33 +2,38 @@ package com.bignerdranch.android.geoquiz
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+
 private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var prevButton: ImageButton
     private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true),
-    )
-    private var currentIndex = 0
+
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this)[QuizViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate called")
         setContentView(R.layout.activity_main)
+
+        val savedCurrentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        val quizViewModel = ViewModelProviders.of(this)[QuizViewModel::class.java]
+        quizViewModel.currentIndex = savedCurrentIndex
+        Log.d(TAG, "Get a QuizViewModel: $quizViewModel")
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -43,49 +48,84 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
         prevButton.setOnClickListener {
-            updateQuestion(--currentIndex)
+            updateQuestion(-1)
         }
         nextButton.setOnClickListener {
-            updateQuestion(++currentIndex)
+            updateQuestion(1)
         }
         questionTextView.setOnClickListener {
-            updateQuestion(++currentIndex)
+            updateQuestion(1)
         }
-        updateQuestion(currentIndex)
+        questionTextView.setText(quizViewModel.currentQuestionText)
     }
 
-    private fun updateQuestion(idx: Int) {
-        this.currentIndex = (idx+questionBank.size) % questionBank.size
-        val questionTextResId = questionBank[currentIndex].textResId
-        questionTextView.setText(questionTextResId)
+    // step 步长
+    private fun updateQuestion(step: Int) {
+        quizViewModel.setCurrentQuestionAnswered()
+        val sz = quizViewModel.questionBank.size
+        var i = 0
+        var idx = quizViewModel.currentIndex
+        while (i < sz) {
+            i++
+            idx = (idx + step + sz) % sz
+            if (!quizViewModel.questionBank[idx].answered) { // 未答过，符合要求
+                break
+            }
+        }
+        if (idx == quizViewModel.currentIndex) {
+            Toast.makeText(this, "您已答完所有题，无题可答了！", Toast.LENGTH_LONG).show()
+        } else {
+            quizViewModel.currentIndex = idx
+            val questionTextResId = quizViewModel.currentQuestionText
+            questionTextView.setText(questionTextResId)
+            var answered = 0 // 已回答的题目数量
+            for (q in quizViewModel.questionBank) {
+                if (q.answered) {
+                    answered++
+                }
+            }
+            Toast.makeText(this, "您已回答了 $answered/$sz 个题目", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
         } else {
             R.string.incorrect_toast
         }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        val t = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+        t.setGravity(Gravity.TOP, 0, 0)
+        t.show()
     }
 
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart called")
     }
+
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume called")
     }
+
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause called")
     }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    }
+
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop called")
     }
+
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy called")
---
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -0,0 +1,42 @@
+package com.bignerdranch.android.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+    init {
+        Log.d(TAG, "ViewModel instance created")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d(TAG, "ViewModel instance about to be destroyed")
+    }
+
+    val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true),
+    )
+    var currentIndex = 0
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext(step: Int) {
+        val sz = questionBank.size
+        currentIndex = (currentIndex + step + sz) % sz
+    }
+
+    fun setCurrentQuestionAnswered() {
+        questionBank[currentIndex].answered = true
+    }
+}
